7d1cb001a1180bc4eadf2b4fa1095228e4cd94d8
deepanshupal09-datazip <deepanshu@datazip.io>
2025-10-29 14:55:26 +0530

    fix: config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   url: 'https://deepanshupal09-datazip.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/olake-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
